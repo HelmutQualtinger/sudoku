0a0e94b228991f389bbb4c21700e4016da08d50f
Helmut Qualtinger <94598493+HelmutQualtinger@users.noreply.github.com>
2024-01-29 15:35:18 +0100

    Start giving color hints, add labels allowing to indicate where numbers are, were they cannot be. Active when hovered on

--- sudoku_tk.py
+++ sudoku_tk.py
@@ -1,13 +1,15 @@
 # pylint: disable=unused-argument
 import tkinter as tk
 from tkinter import Menu
+from tkinter import filedialog
+import itertools
 import sys
 import sudoku as s
 
 
 class SudokuTk(tk.Tk):
     """
-    The Sudoku game class.
+    The Sudoku GUI interface game class.
     """
 
     def __init__(self, board):
@@ -23,9 +25,15 @@ class SudokuTk(tk.Tk):
         """
         super().__init__()
         self.board = board
+        self.active_number = 0
         self.create_widgets()
+        self.update_widgets()
         
-    
+        
+    def set_active_number(self, number):
+        self.active_number = number
+        self.update_widgets()
+        
     def set_field(self, event, row, col, press):
         print (event, row, col, press)
         if not press:
@@ -34,11 +42,19 @@ class SudokuTk(tk.Tk):
             return
         self.Buttons[(row, col)].config(relief=tk.SUNKEN, bg="#00ff00")
         popup_menu = Menu(self, tearoff=0)
-        for i in range(10):
-            popup_menu.add_command(label=str(i+1))
+        for i in self.board.get_candidates(row, col)|{42,0}:
+            if i == 0:
+                label="Clear"
+            else:
+                label=str(i)
+            popup_menu.add_command(label=label
+                                   , command=lambda row=row, col=col, i=i: self.board.set_field(row, col, i))
         popup_menu.post(event.x_root, event.y_root)
+        print("posted", row, col)
         print("set_field", row, col)
-        s
+        self.active_number = self.board.board2d[row, col]
+        self.update_widgets()
+        
     def create_widgets(self):
         """
         Creates the widgets for the Sudoku game.
@@ -46,10 +62,33 @@ class SudokuTk(tk.Tk):
         Returns:
             None
         """
+        # create TOP menu with load save buttons and candidate list
+        self.top_frame = tk.Frame(self, borderwidth=2)
+        self.top_frame.grid(row=0, column=0, columnspan=9, padx=5, pady=5)
+        self.numbers = []
+        
+        self.load_button = tk.Button(self.top_frame, text="Load",
+                                    command=self.load_file)
+        self.load_button.pack(side=tk.LEFT, padx=1, pady=1)
+        for i in range(9):
+            ll=tk.Label(self.top_frame, text=str(i+1),relief=tk.FLAT,
+                        font=("Helvetica", 14, "italic"))
+            ll.pack(side=tk.LEFT, padx=1, pady=1)
+            ll.bind("<Enter>", lambda event, i=i+1:self.set_active_number(i))
+            ll.bind("<Leave>", lambda event, i=i+1:self.set_active_number(0))
+            self.numbers.append(ll)
+            
+            
+        self.save_button = tk.Button(self.top_frame, text="Save", 
+                                    command= self.save_file)
+        self.save_button.pack(side=tk.LEFT, padx=1, pady=1)
+
+        self.button_frame = tk.Frame(self, borderwidth=2, relief="solid")
+        self.button_frame.grid(row=1, column=0, columnspan=9, padx=5, pady=5)
         Button = dict()
         for super_row in range(3):
             for super_col in range(3):
-                frame = tk.Frame(self, borderwidth=2, relief="solid")
+                frame = tk.Frame(self.button_frame, borderwidth=2, relief="solid")
                 frame.grid(row=super_row, column=super_col, padx=5, pady=5)
                 for i in range(3):
                     for j in range(3):
@@ -84,13 +123,49 @@ class SudokuTk(tk.Tk):
                                                 fg="#ffffff", font=("Arial", 18, "bold"))
 
         self.Buttons = Button
-        self.button = tk.Button(self, text="Quit", command=self.quit)
-        self.button.grid(row=9, column=0, columnspan=9)
+        self.qbutton = tk.Button(self, text="Quit", command=self.quit)
+        self.qbutton.grid(row=9, column=0, columnspan=9, sticky="nsew")
+        self.qbutton.grid_rowconfigure(0, weight=1)
+        self.qbutton.grid_columnconfigure(0, weight=1)
+
+        self.title("Assisted Sudoku")
         
 
+    def update_widgets(self):
+        for (row,col) in itertools.product(range(9), range(9)):  # over all rows and columns
 
- 
-        
+            digit_str=str(self.board.board2d[row,col] if self.board.board2d[row,col] else "")
+            # default sunken black on white
+            self.Buttons[(row,col)].config(text=digit_str,                                            
+                            bg="#ffffff", 
+                            fg="#000000",
+                            relief=tk.SUNKEN)
+            if self.board.board2d[row,col] == 0:        # empty fields are light green
+                self.Buttons[(row,col)].config(text=digit_str)  # set the number 
+                self.Buttons[(row,col)].config(bg="#ffffff", 
+                                               fg="#80ff80",
+                                               relief=tk.RAISED)
+            if self.board.frozen2d[row,col]:                 # frozen fields are gray
+                self.Buttons[(row,col)].config(text=str(self.board.board2d[row,col]), 
+                                               bg="#808080", 
+                                               fg="#000000",
+                                               relief=tk.SUNKEN)
+            elif self.board.board2d[row,col] == 0:        # empty fields are light green
+                self.Buttons[(row,col)].config(text="", 
+                                               bg="#ffffff", 
+                                               fg="#80ff80",
+                                               relief=tk.RAISED)
+            else:
+                self.Buttons[(row,col)].config(text=str(self.board.board2d[row,col]),  # filled fields are white
+                                               bg="#ffffff", 
+                                               fg="#000000",
+                                               relief=tk.SUNKEN)
+            if (not self.active_number == 0 and 
+                not self.board.board2d[row,col] in self.board.get_candidates(row,col)): # candidates are light red
+                self.Buttons[(row,col)].config(bg="#ff8080")
+            if self.board.board2d[row,col] == self.active_number: # active number is red
+                self.Buttons[(row,col)].config(bg="#c00000")
+    
     def quit(self):
         """
 
@@ -99,6 +174,20 @@ class SudokuTk(tk.Tk):
             None
         """
         sys.exit(0)
+        
+    def load_file(self,filename=None, ignore_discard=False, ignore_expires=False):
+ 
+            filename = filedialog.askopenfilename(filetypes=[("JSON Files", "*.json")])
+            if filename:
+                print ("Loading file",filename)
+                self.board.load_json(filename)
+                self.update_widgets()
+            
+    def save_file(self,filename=None, ignore_discard=False, ignore_expires=False):
+        filename = filedialog.asksaveasfilename(filetypes=[("JSON Files", "*.json")])
+        if filename:
+            print("Saving file",filename)
+            self.board.save_json(filename)
 
     def report_callback_exception(self, exc, val, tb, *args):
         print("report_callback_exception", exc, val, tb)
@@ -106,7 +195,8 @@ class SudokuTk(tk.Tk):
 
 
 
-root = tk.Tk()
+# root = tk.Tk()
+# root.title("Sudoku")
 
 
 sudoku = s.Sudoku(s.EMPTY_BOARD)
